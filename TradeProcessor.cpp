540b6aabca7a7398d361bd65f9f15c8851ee7992
MohebaIbu <33979358+MohebaIbu@users.noreply.github.com>
2024-06-13 16:23:50 +0530

    Moved conversion code to seperate header TradeProcessor.cpp

--- TradeProcessor.cpp
+++ TradeProcessor.cpp
@@ -3,24 +3,6 @@
 #include <string.h>
 #include <errno.h>
 
-int tryToConvertStringToInt(const char* inputString, int* integerValue) {
-    char* endptr;
-    *integerValue = strtol(inputString, &endptr, 10);
-    if (endptr == inputString) {
-        return 0;
-    }
-    return 1;
-}
-
-int tryToConvertStringToDecimal(const char* inputString, double* decimalValue) {
-    char* endptr;
-    *decimalValue = strtod(inputString, &endptr);
-    if (endptr == inputString) {
-        return 0;
-    }
-    return 1;
-}
-
 void convertCsvToXmlFile(FILE* csvfilestream) {
     char csvfileline[1024];
     TradeRecords records[1024];
