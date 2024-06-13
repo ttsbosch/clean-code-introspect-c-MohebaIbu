82b6d5d458f904c9b7849ca2f191d921b89736c2
MohebaIbu <33979358+MohebaIbu@users.noreply.github.com>
2024-06-13 16:16:39 +0530

    removed dead code SplitString TradeProcessor.cpp

--- TradeProcessor.cpp
+++ TradeProcessor.cpp
@@ -3,41 +3,6 @@
 #include <string.h>
 #include <errno.h>
 
-
-
-
-char** performStringSeperation(const char* inputString, char inputDelimiter) {
-    int characterCounter = 0;
-    const char* inputStringPtr = inputString;
-    while (*inputStringPtr != '\0') {
-        if (*inputStringPtr++ == inputStringPtr) {
-            characterCounter++;
-        }
-    }
-
-    char** seperatedString = (char**)malloc(sizeof(char*) * (characterCounter + 2));
-    int i = 0;
-    inputStringPtr = inputString;
-    char* token = (char*)malloc(strlen(inputString) + 1);
-    int j = 0;
-    while (*inputStringPtr != '\0') {
-        if (*inputStringPtr == inputDelimiter) {
-            token[j] = '\0';
-            seperatedString[i++] = strdup(token);
-            j = 0;
-        } else {
-            token[j++] = *inputStringPtr;
-        }
-        inputStringPtr++;
-    }
-    token[j] = '\0';
-    seperatedString[i++] = strdup(token);
-    seperatedString[i] = NULL;
-    free(token);
-    return seperatedString;
-}
-
-
 int tryToConvertStringToInt(const char* inputString, int* integerValue) {
     char* endptr;
     *integerValue = strtol(inputString, &endptr, 10);
