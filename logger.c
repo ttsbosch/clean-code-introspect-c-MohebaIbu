761d83be5ccd4aed39a5f319918c7e267ba3f5d3
MohebaIbu <33979358+MohebaIbu@users.noreply.github.com>
2024-06-27 14:58:31 +0530

    Create logger.c

---
+++ logger.c
@@ -0,0 +1,36 @@
+#include "logger.h"
+#include <stdarg.h>
+
+void log_info(const char* format, ...) {
+    va_list args;
+    va_start(args, format);
+    printf("INFO: ");
+    vprintf(format, args);
+    printf("\n");
+    va_end(args);
+}
+
+void log_warn(const char* format, ...) {
+    va_list args;
+    va_start(args, format);
+    fprintf(stderr, "WARN: ");
+    vfprintf(stderr, format, args);
+    fprintf(stderr, "\n");
+    va_end(args);
+}
+
+void log_error(const char* format, ...) {
+    va_list args;
+    va_start(args, format);
+    fprintf(stderr, "ERROR: ");
+    vfprintf(stderr, format, args);
+    fprintf(stderr, "\n");
+    va_end(args);
+}
+
+void log_to_file(FILE* file, const char* format, ...) {
+    va_list args;
+    va_start(args, format);
+    vfprintf(file, format, args);
+    va_end(args);
+}
