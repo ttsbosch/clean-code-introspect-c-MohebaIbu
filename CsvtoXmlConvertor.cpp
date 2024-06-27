48264aa6f0316e69cd4da7d156cad2e39f6dcfc9
MohebaIbu <33979358+MohebaIbu@users.noreply.github.com>
2024-06-27 18:35:27 +0530

    Update CsvtoXmlConvertor.cpp

--- CsvtoXmlConvertor.cpp
+++ CsvtoXmlConvertor.cpp
@@ -3,7 +3,7 @@
 #include <string.h>
 #include <errno.h>
 #include "TradeRecord.h"
-
+#include "ErrorHandling.h"
 #include "StringHelpers.h"
 #include "logger.h"
 #include "IO_Operation.h"
@@ -25,25 +25,7 @@ void convertCsvToXmlFile(FILE* csvfilestream) {
             token = strtok(NULL, ",");
         }
 
-        if (csvFieldCount != 3) {
-            log_warn(stderr, "WARN: Line %d malformed. Only %d field(s) found.\n", lineCount + 1, csvFieldCount);
-            continue;
-        }
-
-        if (strlen(csvFileFields[0]) != 6) {
-            log_warn(stderr, "WARN: Trade currencies on line %d malformed: '%s'\n", lineCount + 1, csvFileFields[0]);
-            continue;
-        }
-
-        int tradeAmount;
-        if (!tryToConvertStringToInt(csvFileFields[1], &tradeAmount)) {
-            log_warn(stderr, "WARN: Trade amount on line %d not a valid integer: '%s'\n", lineCount + 1, csvFileFields[1]);
-        }
-
-        double tradePrice;
-        if (!tryToConvertStringToDecimal(csvFileFields[2], &tradePrice)) {
-            log_warn(stderr, "WARN: Trade price on line %d not a valid decimal: '%s'\n", lineCount + 1, csvFileFields[2]);
-        }
+        performErrorChecks(csvFieldCount,lineCount ,ccsvFileFields)
         
         strncpy(records[recordCount].SrcCurrency, csvFileFields[0], 3);
         strncpy(records[recordCount].DestCurrency, csvFileFields[0] + 3, 3);
