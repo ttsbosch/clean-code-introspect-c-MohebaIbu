798227990f5b6f991a137b91aa6868e6b0f5737a
MohebaIbu <33979358+MohebaIbu@users.noreply.github.com>
2024-06-27 15:05:15 +0530

    Update CsvtoXmlConvertor.cpp

--- CsvtoXmlConvertor.cpp
+++ CsvtoXmlConvertor.cpp
@@ -5,6 +5,29 @@
 #include "TradeRecord.h"
 #include "DataTypeConvertor.h"
 #include "StringHelpers.h"
+#include "logger.h"
+
+
+void write_trade_records_to_xml(const TR* objects, int objectCount, const char* filename) {
+    FILE* outFile = fopen(filename, "w");
+    if (outFile == NULL) {
+        log_error("Failed to open output file");
+        return;
+    }
+
+    log_to_file(outFile, "<TradeRecords>\n");
+    for (int i = 0; i < objectCount; i++) {
+        log_to_file(outFile, "\t<TradeRecord>\n");
+        log_to_file(outFile, "\t\t<SourceCurrency>%s</SourceCurrency>\n", objects[i].SC);
+        log_to_file(outFile, "\t\t<DestinationCurrency>%s</DestinationCurrency>\n", objects[i].DC);
+        log_to_file(outFile, "\t\t<Lots>%.2f</Lots>\n", objects[i].Lots);
+        log_to_file(outFile, "\t\t<Price>%.2f</Price>\n", objects[i].Price);
+        log_to_file(outFile, "\t</TradeRecord>\n");
+    }
+    log_to_file(outFile, "</TradeRecords>");
+    fclose(outFile);
+    log_info("%d trades processed and written to %s", objectCount, filename);
+}
 
 void convertCsvToXmlFile(FILE* csvfilestream) {
     char csvfileline[1024];
@@ -49,18 +72,6 @@ void convertCsvToXmlFile(FILE* csvfilestream) {
         lineCount++;
     }
 
-    FILE* outputXmlFile = fopen("output.xml", "w");
-    fprintf(outputXmlFile, "<TradeRecords>\n");
-    for (int recordIndex = 0; recordIndex < recordCount; recordIndex++) {
-        fprintf(outputXmlFile, "\t<TradeRecord>\n");
-        fprintf(outputXmlFile, "\t\t<SourceCurrency>%s</SourceCurrency>\n", records[recordIndex].SrcCurrency);
-        fprintf(outputXmlFile, "\t\t<DestinationCurrency>%s</DestinationCurrency>\n", records[recordIndex].DestCurrency);
-        fprintf(outputXmlFile, "\t\t<Lots>%d</Lots>\n", records[recordIndex].Lots);
-        fprintf(outputXmlFile, "\t\t<Price>%f</Price>\n", records[recordIndex].Price);
-        fprintf(outputXmlFile, "\t</TradeRecord>\n");
-    }
-    fprintf(outputXmlFile, "</TradeRecords>");
-    fclose(outputXmlFile);
-    printf("INFO: %d trades processed\n", recordCount);
+    write_trade_records_to_xml(objects, objectCount, "output.xml");
 }
 
