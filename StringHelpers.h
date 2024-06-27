0d157f0655a4ad41623708ed1e4f4faae8148db9
MohebaIbu <33979358+MohebaIbu@users.noreply.github.com>
2024-06-27 14:31:24 +0530

    Update StringHelpers.h

--- StringHelpers.h
+++ StringHelpers.h
@@ -10,9 +10,10 @@ char** performStringSeperation(const char* inputString, char inputDelimiter) {
     int characterCounter = 0;
     const char* inputStringPtr = inputString;
     while (*inputStringPtr != '\0') {
-        if (*inputStringPtr++ == inputStringPtr) {
+        if (*inputStringPtr == inputStringPtr) {
             characterCounter++;
         }
+        inputStringPtr++;
     }
 
     char** seperatedString = (char**)malloc(sizeof(char*) * (characterCounter + 2));
