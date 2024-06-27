52bbe2ac0cd9df74c5cde718080c78d1529eee25
MohebaIbu <33979358+MohebaIbu@users.noreply.github.com>
2024-06-27 15:40:28 +0530

    Delete CsvtoXmlConvertorInterface.h

--- CsvtoXmlConvertorInterface.h
+++
@@ -1,9 +0,0 @@
-#ifndef CSVTOXMLCONVERTORINTERFACE_H
-#define CSVTOXMLCONVERTORINTERFACE_H
-
-#include "TradeRecord.h"
-
-// Function prototypes
-void write_trade_records_to_xml(const TR* objects, int objectCount, const char* filename);
-
-#endif 
