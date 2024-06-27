316203e25ab023d00e77bddfa12a3af534c34467
MohebaIbu <33979358+MohebaIbu@users.noreply.github.com>
2024-06-27 18:48:13 +0530

    Update CsvtoXmlConvertor.cpp

--- CsvtoXmlConvertor.cpp
+++ CsvtoXmlConvertor.cpp
@@ -2,13 +2,53 @@
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
+#include "DataTypeConvertor.h"
+#include "logger.h"
 #include "TradeRecord.h"
 #include "ErrorHandling.h"
 #include "StringHelpers.h"
-#include "logger.h"
 #include "IO_Operation.h"
 
+void  checkFieldCountAsSpecified (int csvFieldCount,int lineCount,int refCountValue)
+{
+  if (csvFieldCount != refCountValue) {
+            log_warn(stderr, "WARN: Line %d malformed. Only %d field(s) found.\n", lineCount + 1, csvFieldCount);
+            
+        }
+}
 
+void  checkLengthOfieldsAsSpecified (char *csvFileFields[3],int lineCount,int refFieldLength)
+{
+   if (strlen(csvFileFields[0]) != refFieldLength) {
+            log_warn(stderr, "WARN: Trade currencies on line %d malformed: '%s'\n", lineCount + 1, csvFileFields[0]);
+            
+        }
+}
+
+void  checkGivenVaribleIsInt (char *csvFileFields[3],int lineCount)
+{
+  int tradeAmount;
+  if (!tryToConvertStringToInt(csvFileFields[1], &tradeAmount)) {
+      log_warn(stderr, "WARN: Trade amount on line %d not a valid integer: '%s'\n", lineCount + 1, csvFileFields[1]);
+    }
+}
+
+void  checkGivenVaraibleIsDouble (char *csvFileFields[3],int lineCount)
+{
+  double tradePrice;
+  if (!tryToConvertStringToDecimal(csvFileFields[2], &tradePrice)) 
+  {
+    log_warn(stderr, "WARN: Trade price on line %d not a valid decimal: '%s'\n", lineCount + 1, csvFileFields[2]);
+  }
+}
+
+void performErrorChecks(int csvFieldCount,int lineCount ,char *csvFileFields[3] )
+{
+  checkFieldCountAsSpecified(csvFieldCount,lineCount,3);
+  checkLengthOfieldsAsSpecified(csvFileFields,lineCount,6);
+  checkGivenVaribleIsInt(csvFileFields,lineCount);
+  checkGivenVaraibleIsDouble(csvFileFields,lineCount);
+}
 
 void convertCsvToXmlFile(FILE* csvfilestream) {
     char csvfileline[1024];
