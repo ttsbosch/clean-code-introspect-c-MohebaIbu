cf298d2bd13fde1a9076732efec9c922a1542d5e
MohebaIbu <33979358+MohebaIbu@users.noreply.github.com>
2024-06-27 15:17:07 +0530

    Create IO_Operation.h

---
+++ IO_Operation.h
@@ -0,0 +1,9 @@
+#ifndef IO_OPERATION_H
+#define IO_OPERATION_H
+
+#include "mylibrary.h"
+
+// Function prototypes
+void write_trade_records_to_xml(const TR* objects, int objectCount, const char* filename);
+
+#endif 
